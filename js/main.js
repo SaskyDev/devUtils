72c14eeadfc59a8d81148f50af529ba758ab2a7f
SaskyDev <alexgomez96_@hotmail.com>
2026-03-10 21:10:28 +0100

    Add password generator tool

--- js/main.js
+++ js/main.js
@@ -121,3 +121,23 @@ function testRegex() {
     }
 
 }
+
+function generatePassword() {
+
+    const length = document.getElementById("length").value;
+
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+
+        const randomIndex = Math.floor(Math.random() * chars.length);
+
+        password += chars[randomIndex];
+
+    }
+
+    document.getElementById("output").textContent = password;
+
+}
